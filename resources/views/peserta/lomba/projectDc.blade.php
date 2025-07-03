f9d06b1d3189792c0a02c632b385766dd7a6e009
syahfrinorezky <syahfrinorezky@gmail.com>
2025-07-04 03:13:53 +0800

    ubah countdown pengumpulan project

--- resources/views/peserta/lomba/projectDc.blade.php
+++ resources/views/peserta/lomba/projectDc.blade.php
@@ -151,7 +151,7 @@
 </div>
 <script>
     // Set the date we're counting down to
-    var countDownDate = new Date("September 06, 2025 23:59:59").getTime();
+    var countDownDate = new Date("September 02, 2025 23:59:59").getTime();
     countDownDate.toLocaleString("en-US", {timeZone: "Asia/Singapore"});
     // Update the count down every 1 second
     var x = setInterval(function() {            
