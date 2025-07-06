08efd79ff9360ad43886364963db16ff0d5a35f5
syahfrinorezky <syahfrinorezky@gmail.com>
2025-07-06 09:51:08 +0800

    perbaiki typo sekali lagi

--- resources/views/panitia/layout/sidebar.blade.php
+++ resources/views/panitia/layout/sidebar.blade.php
@@ -46,7 +46,7 @@
                     <li>
                         <a href="{{url('/ctf-panitia')}}"
                             class="flex gap-2 items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                            <img src="{{ asset('images/logo/ctf-vector.svg') }}" alt="logo dc" class="w-6 h-6">
+                            <img src="{{ asset('images/logo/ctf-vector.svg') }}" alt="logo ctf" class="w-6 h-6">
                             <span>CTF</span>
                         </a>
                     </li>
